a6bbf43241958a21528276fe2b38619c94b8d254
Swati Dubey <swatidubey914@gmail.com>
2022-04-19 18:11:13 +0530

    Node.js

---
+++ Node.js
@@ -0,0 +1,14 @@
+Node.js -->>
+            Node.js is an open source server environment
+            Node.js is free
+            Node.js runs on various platforms (Windows, Linux, Unix, Mac OS X, etc.)
+            Node.js uses JavaScript on the server
+            Node.js is a server-side platform built on Google Chrome's JavaScript Engine (V8 Engine). 
+            Node.js was developed by Ryan Dahl in 2009 and its latest version is v0.10.36.  
+            Node.js = Runtime Environment + JavaScript Library
+            Node.js applications are written in JavaScript, and can be run within the Node.js runtime on OS X, Microsoft Windows, and Linux.
+            Node.js also provides a rich library of various JavaScript modules which simplifies the development of web applications using Node.js to a great extent.
+            
+Where Not to Use Node.js? -->>
+            It is not advisable to use Node.js for CPU intensive applications.
+
